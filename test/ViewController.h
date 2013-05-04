984e5ead183c8fe7c780172caa50a2eadb6aa8dd
zaq6111 <siriphan.at@gmail.com>
2013-05-04 16:01:00 +0700

    แปลงค่าเงิน

--- test/ViewController.h
+++ test/ViewController.h
@@ -9,5 +9,15 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UISwitch *mySwitch;
+@property (strong, nonatomic) IBOutlet UITextField *myInput;
+@property (strong, nonatomic) IBOutlet UISlider *mySlider;
+@property (strong, nonatomic) IBOutlet UILabel *myRate;
+- (IBAction)myButton:(id)sender;
+
+@property (strong, nonatomic) IBOutlet UILabel *myOutput;
+- (IBAction)SliderMoveing:(id)sender;
+- (IBAction)mySwitch:(id)sender;
+@property (strong, nonatomic) IBOutlet UISlider *slider;
 
 @end
--- test/ViewController.m
+++ test/ViewController.m
@@ -26,4 +26,24 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)myButton:(id)sender {
+    _myOutput.text=[NSString stringWithFormat:@"%.2f",_myInput.text.floatValue*_myRate.text.floatValue ];
+    [self.view endEditing:YES];
+   
+}
+- (IBAction)SliderMoveing:(id)sender {
+    _myRate.text=[NSString stringWithFormat:@"%.2f",_mySlider.value*100];
+   
+}
+
+- (IBAction)mySwitch:(id)sender {
+    if(_mySwitch.on==YES){
+        _mySlider.hidden=NO;
+        
+    }
+    else{
+        _mySlider.hidden=YES;
+        _myRate.text=@"30.0";
+    }
+}
 @end
